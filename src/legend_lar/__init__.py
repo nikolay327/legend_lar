bfd0c44594ddada5cfe6fb449d6170f7dcb07836
Niko Nanda Putra Nila Lay <ge35miw@sator.e15.ph.tum.de>
2026-03-28 16:02:00 +0100

    cleanup outdated training and calibration codes

--- src/legend_lar/__init__.py
+++ src/legend_lar/__init__.py
@@ -1,12 +1,2 @@
 import importlib.metadata
 __version__ = importlib.metadata.version("legend_lar")
-
-from .bce_trainer import train_bce
-from .conditional_trainer import train_conditional
-from .unconditional_trainer import train_unconditional
-
-__all__ = [
-    "train_bce",
-    "train_conditional",
-    "train_unconditional"
-]
--- src/legend_lar/calibration/__init__.py
+++ src/legend_lar/calibration/__init__.py
@@ -1,7 +1,5 @@
 from .ece import NRETestMetrics
-from .type1 import evaluate
 
 __all__ = [
-    "NRETestMetrics",
-    "evaluate"
+    "NRETestMetrics"
 ]
