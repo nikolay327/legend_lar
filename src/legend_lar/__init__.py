907810d2facf2e7c2e10c2366098e01fd1e8da1f
Niko Nanda Putra Nila Lay <ge35miw@sator.e15.ph.tum.de>
2026-04-05 15:17:10 +0200

    create API for nre_c training and base dataset creation

--- src/legend_lar/__init__.py
+++ src/legend_lar/__init__.py
@@ -1,2 +1,10 @@
 import importlib.metadata
 __version__ = importlib.metadata.version("legend_lar")
+
+from .kfold_ensemble import nre_c
+from .utils import create_base_dataset
+
+__all__ = [
+    "create_base_dataset",
+    "nre_c"
+]
