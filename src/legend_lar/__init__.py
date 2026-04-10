dd578948ac887b368b238313d0b1230db44cd938
Niko Nanda Putra Nila Lay <ge35miw@sator.e15.ph.tum.de>
2026-04-10 10:48:59 +0200

    rename and create an direct api for nrec inference script

--- src/legend_lar/__init__.py
+++ src/legend_lar/__init__.py
@@ -4,11 +4,14 @@ __version__ = importlib.metadata.version("legend_lar")
 from pathlib import Path
 __path__.append(str(Path(__file__).resolve().parent / "kfold_ensemble"))
 __path__.append(str(Path(__file__).resolve().parent / "utils"))
+__path__.append(str(Path(__file__).resolve().parent / "calibration"))
 
 from .kfold_ensemble import nre_c
 from .utils import create_base_dataset
+from .calibration import nre_c_inference
 
 __all__ = [
     "create_base_dataset",
-    "nre_c"
+    "nre_c",
+    "nre_c_inference"
 ]
